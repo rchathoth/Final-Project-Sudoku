a078ae0f7add43e1b234a38ea6ebfb98693e83b2
Your Name <you@example.com>
2025-12-01 15:00:14 -0500

    main

---
+++ sudoku.py
@@ -0,0 +1,17 @@
+import pygame as game
+
+def main():
+    game.init()
+    screen = game.display.set_mode((600, 600))
+    game.display.set_caption("Sudoku")
+    running = True
+    while running:
+        for event in game.event.get():
+            if event.type == game.QUIT:
+                running = False
+    game.quit()
+
+
+
+if __name__ == "__main__":
+    main()
