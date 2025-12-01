ac06fa897dcb432c546836171c27c33ba3cffb5d
adp0419 <adp0419@gmail.com>
2025-12-01 15:46:31 -0500

    adp

--- board.py
+++ board.py
@@ -1,19 +1,9 @@
-import pygame
-
 class Board():
     def __init__(self, width, height, screen, difficulty):
-        self.width = width
-        self.height = height
-        self.screen = screen
-        self.difficulty = difficulty
+        pass
     
     def draw(self):
-        hi = self.height / 9
-        wi = self.width / 9
-        for i in range(9):
-            pygame.draw.line(self.screen, "black", (i*wi, 0), (i*wi, self.height) )
-        
-        
+        pass
 
     def select(self, row, col):
         pass
@@ -26,22 +16,33 @@ class Board():
  
     def sketch(self, value):
         pass
-     
+    
     def place_number(self, value):
         pass
     
     def reset_to_original(self):
-        pass
+        for r in range(self.rows):
+            for c in range(self.cols):
+                self.model[r][c] = self.original_board[r][c]
    
     def is_full(self):
-        pass
+        for row in self.model:
+            for val in row:
+                if val == 0:
+                    return False
+        return True
 
     def update_board(self):
-        pass
+        for r in range(self.rows):
+            for c in range(self.cols):
+                self.model[r][c] = self.cells[r][c].value
 
     def find_empty(self):
-        pass
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.model[r][c] == 0:
+                    return (r, c)
+        return None
 
     def check_board(self):
-        pass
-
+        print("hello world")    
